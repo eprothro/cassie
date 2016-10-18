8dc97eefc3428f2e6a3fc619eb42054b50a366e4
Evan Prothro <evan.prothro@gmail.com>
2016-10-18 13:08:39 -0500

    Bump to 1.0.0.beta.24

---
+++ CHANGELOG.md
@@ -0,0 +1,50 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+### Added
+- `Cassie::Support::CommandRunner#run!`
+
+## 1.0.0.beta.24
+
+### Added
+- `Cassie::Result::QueryResult.first!`
+- `to_cql` method to Statements, providing CQL strings that are executable in `cqlsh`
+
+## 1.0.0.beta.22
+
+### Added
+- `Cassie::Statements.default_consistency`, fallback for query classes
+
+## 1.0.0.beta.21
+
+### Added
+- select support with `select` method and helpers (`writetime`, `count`, etc)
+- dynamic term support and non-positional terms (`now()`, `minTimeUuid(?)`, etc)
+- separate `Cassandra::Query`, `Cassandra::Definition`, and `Cassandra::Modification` base classes (see Readmes)
+- `cassie tail` command
+- `cassie stop``--all` switch
+- support for synthetic partition linking (see Readmes)
+- `Cassie::Statements.default_limit`
+
+### Changed
+- breaking: rename `build_resource` to `build_result`
+- breaking: rename `select` to `select_from`
+- breaking: moved `next_row` out of query and into `result`, now named `peeked_row`
+- breaking: moved `next_max_cursor` and `next_max_<identifier>` out of query object and into `result` object
+- deprecate `insert` in favor of `insert_into`
+- deprecate `delete` in favor of `delete_from`
+- query results are now an enumerable object decorating Cassandra::Results object, accessable via `result` after execution, or directly via `fetch`
+- `value` options for `set` and `where` DSL now only accepts a symbol for a method to call. (no longer supports string eval or static values)
+
+### Removed
+- `insert` alias for execute
+- `update` alias for execute
+- `delete` alias for execute
+- `select` alias for execute
+- `Cassie::Statements::Statement::Pagination::PageSize.default`, use `Cassie::Statements.default_limit` instead.
--- lib/cassie/support/command_runner.rb
+++ lib/cassie/support/command_runner.rb
@@ -1,69 +1,80 @@
-module Cassie::Support
-  class CommandRunner
-    attr_reader :binary, :args, :command, :status, :duration, :output
+module Cassie
+  module Support
+    class CommandRunner
+      attr_reader :binary, :args, :command, :status, :duration, :output
 
-    # When a block is given, the command runs before yielding
-    def initialize(binary, args=[])
-      @binary = binary
-      @args = args
-      @command = (Array(binary) + args).join(" ")
-      @command = command + " 2>&1" unless command =~ / > /
+      # When a block is given, the command runs before yielding
+      def initialize(binary, args=[])
+        @binary = binary
+        @args = args
+        @command = (Array(binary) + args).join(" ")
+        @command = command + " 2>&1" unless command =~ / > /
 
-      if block_given?
-        run
-        yield self
-      end
-    end
-
-    # Runs the command
-    def run
-      t1=Time.now
-
-      IO.popen(command) do |io|
-        @output=io.read
-        @status=Process.waitpid2(io.pid)[1]
+        if block_given?
+          run
+          yield self
+        end
       end
 
-      @duration=Time.now-t1
-      completed?
-    end
+      # Runs the command
+      def run
+        t1=Time.now
 
-    # Returns false if the command hasn't been executed yet
-    def run?
-      !!@duration
-    end
+        IO.popen(command) do |io|
+          @output=io.read
+          @status=Process.waitpid2(io.pid)[1]
+        end
 
-    # Returns the exit code for the command. Runs the command if it hasn't run yet.
-    def exitcode
-      status.exitstatus
-    end
+        @duration=Time.now-t1
+        completed?
+      end
 
-    # Returns true if exited 0
-    def success?
-      return false unless run?
-      exitcode == 0
-    end
+      # Runs the command, raising if program
+      # was not run successfully
+      def run!
+        unless run && success?
+          raise RuntimeError.new(failure_message)
+        end
+        true
+      end
 
-    # Returns true if the command completed execution and success bits were set.
-    # Will return false if the command hasn't been executed
-    def completed?
-      return false unless run?
-      status.exited? && @status.success?
-    end
+      # Returns false if the command hasn't been executed yet
+      def run?
+        !!@duration
+      end
 
-    def failure_message
-      msg = "---------------------\n"
-      msg << red(output)
-      msg << "---------------------\n"
-      msg << "Failed to execute `#{command}`:\n"
-      msg << "\tPlease check the output above for any errors and make sure that `#{binary}` is installed in your PATH with proper permissions."
-      msg
-    end
+      # Returns the exit code for the command. Runs the command if it hasn't run yet.
+      def exitcode
+        status.exitstatus
+      end
 
-    protected
+      # Returns true if exited 0
+      def success?
+        return false unless run?
+        exitcode == 0
+      end
 
-    def red(message)
-      "\e[1m\e[31m#{message}\e[0m\e[22m"
+      # Returns true if the command completed execution and success bits were set.
+      # Will return false if the command hasn't been executed
+      def completed?
+        return false unless run?
+        status.exited? && @status.success?
+      end
+
+      def failure_message
+        msg = "---------------------\n"
+        msg << red(output)
+        msg << "---------------------\n"
+        msg << "Failed to execute `#{command}`:\n"
+        msg << "\tPlease check the output above for any errors and make sure that `#{binary}` is installed in your PATH with proper permissions."
+        msg
+      end
+
+      protected
+
+      def red(message)
+        "\e[1m\e[31m#{message}\e[0m\e[22m"
+      end
     end
   end
 end
